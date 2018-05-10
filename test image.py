d36502e9ced0682b6c717760eb3f8d4748af824a
Julien Seillade <jseillade@gmail.com>
2018-05-10 19:55:25 +0200

    fix

--- test image.py
+++
@@ -1,7 +0,0 @@
-from tkinter import *
-root=Tk()
-toto=PhotoImage(file="oui.png")
-canvas=Canvas(root,width=3000,height=3000)
-canvas.pack()
-canvas.create_image(0,0,anchor=NW,image=toto)
-root.mainloop()
